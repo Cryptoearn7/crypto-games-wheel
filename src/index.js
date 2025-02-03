897d75b9dffa45fdba5a737e219147473b5487c5
Cryptoearn7 <cryptoearn7@gmail.com>
2025-02-03 15:07:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ThreeScene from "./components/ThreeScene.js"; // Import the 3D scene component
+import ThreeScene from "./components/ThreeScene"; // Import the 3D scene component
 
 ReactDOM.render(
   <React.StrictMode>
