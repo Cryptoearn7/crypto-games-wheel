df76442604b2cf72b6ee942c4262ace5ba04060f
Cryptoearn7 <cryptoearn7@gmail.com>
2025-02-03 19:32:05 +0100

    Update ThreeScene.js camera 6

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -9,7 +9,7 @@ function RoomModel() {
 
 export default function ThreeScene() {
   return (
-    <Canvas camera={{ position: [0, 4, 5], fov: 50 }}>
+    <Canvas camera={{ position: [0, 6, 5], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
