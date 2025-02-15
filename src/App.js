9c5b71b780d57364083564ba4b40a955930d018e
Cryptoearn7 <cryptoearn7@gmail.com>
2025-02-15 02:23:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import GamePage from "./components/GamePage";
+import HomePage from "./components/HomePage"; 
+import GamePage from "./components/GamePage"; 
 import "./styles.css";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* 🔹 Landing Page */}
         <Route path="/" element={<HomePage />} />
-
-        {/* 🔹 Game Page */}
         <Route path="/game" element={<GamePage />} />
       </Routes>
     </Router>
