0c3737c93e0331032cbdea5ef34fbe1b37bff952
Cryptoearn7 <cryptoearn7@gmail.com>
2025-02-15 02:26:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ThreeScene from "./components/ThreeScene"; // Import the 3D scene component
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
-      <ThreeScene />
-    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
