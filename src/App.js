a4e113331f9b620a7901306f8d85462dd9c29b7b
Cryptoearn7 <cryptoearn7@gmail.com>
2025-02-15 03:25:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage"; 
-import GamePage from "./components/GamePage"; 
+import HomePage from "./components/HomePage";
+import GamePage from "./components/GamePage";
+import CodeBreaker from "./components/CodeBreaker"; // ✅ New Game
 import "./styles.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/game" element={<GamePage />} />
+        <Route path="/code-breaker" element={<CodeBreaker />} /> {/* ✅ New Route */}
       </Routes>
     </Router>
   );
