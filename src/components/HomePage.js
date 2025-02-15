aeef835f796ad8beaf76cb8216322d4bf4350ae6
Cryptoearn7 <cryptoearn7@gmail.com>
2025-02-15 01:57:31 +0100

    Create HomePage.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+export default function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Crypto Games</h1>
+      <p>The best place for fun crypto-based games and rewards!</p>
+
+      {/* 🚀 Enter the App Button */}
+      <Link to="/game">
+        <button className="enter-button">Enter the App</button>
+      </Link>
+    </div>
+  );
+}
